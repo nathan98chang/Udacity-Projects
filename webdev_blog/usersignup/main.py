0e529a68c561d60676d11d9635624e4c4a2a8660
Nathan Chang <changnat@usc.edu>
2017-12-20 11:46:26 -0800

    added start of user signup problem

--- webdev_blog/usersignup/main.py
+++ webdev_blog/usersignup/main.py
@@ -3,10 +3,25 @@
 # copyright Nathan Chang
 # changnat@usc.edu
 import webapp2
+import re
+
+
+# input verification functions based on given parameters from write-up
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+def valid_username(username):
+    return USER_RE.match(username)
+def valid_password(password):
+    return PASS_RE.match(password)
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
 
 
 class MainPage(webapp2.RequestHandler):
-    
+
 
 
 
