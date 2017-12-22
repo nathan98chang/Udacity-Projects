709210fc93499202d4744e3af4c8b469ff791fa7
Nathan Chang <changnat@usc.edu>
2017-12-21 16:37:27 -0800

    finished html portion

--- webdev_blog/usersignup/main.py
+++ webdev_blog/usersignup/main.py
@@ -18,7 +18,36 @@ def valid_password(password):
 def valid_email(email):
     return EMAIL_RE.match(email)
 
-
+form = """
+<form method = "post">
+    <h2><b>Signup</b></h2>
+    <table>
+      <tbody>
+        <tr>
+        <td><label>Username</label></td>
+        <td><input type = "text" name = "username">%(username)s</td>
+        </tr>
+        
+        <tr>
+        <td><label>Password</label></td>
+        <td><input type = "password" name = "password">%(password)s</td>
+        </tr>
+        
+        <tr>
+        <td><label>Verify Password</label></td>
+        <td><input type = "password" name = "verify">%(verify)s</td>
+        </tr>
+        
+        <tr>
+        <td><label>Email (optional)</label></td>
+        <td><input type = "text" name = "email">%(email)s</td>
+        </tr>
+      </tbody>
+    </table>
+    
+    <input type = "submit">
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
 
