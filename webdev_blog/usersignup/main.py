ab81212478813896b1c542883033a0a3b60567ea
Nathan Chang <changnat@usc.edu>
2017-12-21 17:09:08 -0800

    figuring out how to keep parameters from one page during a redirect

--- webdev_blog/usersignup/main.py
+++ webdev_blog/usersignup/main.py
@@ -5,7 +5,6 @@
 import webapp2
 import re
 
-
 # input verification functions based on given parameters from write-up
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 PASS_RE = re.compile(r"^.{3,20}$")
@@ -18,6 +17,7 @@ def valid_password(password):
 def valid_email(email):
     return EMAIL_RE.match(email)
 
+#HTML code for the form
 form = """
 <form method = "post">
     <h2><b>Signup</b></h2>
@@ -26,21 +26,25 @@ form = """
         <tr>
         <td><label>Username</label></td>
         <td><input type = "text" name = "username">%(username)s</td>
+        <td>%(usererror)s</td>
         </tr>
         
         <tr>
         <td><label>Password</label></td>
         <td><input type = "password" name = "password">%(password)s</td>
+        <td>%(passerror)s</td>
         </tr>
         
         <tr>
         <td><label>Verify Password</label></td>
         <td><input type = "password" name = "verify">%(verify)s</td>
+        <td>%(verifyerror)s</td>
         </tr>
         
         <tr>
         <td><label>Email (optional)</label></td>
         <td><input type = "text" name = "email">%(email)s</td>
+        <td>%(emailerror)s</td>
         </tr>
       </tbody>
     </table>
@@ -49,10 +53,49 @@ form = """
 </form>
 """
 
+#HTML code for success page
+success = """
+<h1><b>Welcome %(username)s!</b></h1>
+"""
+
 class MainPage(webapp2.RequestHandler):
+    def write_form(self, user = "", pw = "", ver = "", eml = "",
+        usererr = "", pwerr = "", verifyerr = "", emailerr = ""):
+        self.response.out.write(form % {"username": user,
+                                        "password": pw,
+                                        "verify": ver,
+                                        "email": eml,
+                                        "usererror": usererr,
+                                        "passerror": pwerr,
+                                        "verifyerror": verifyerr,
+                                        "emailerror": emailerr})
+
+    def get(self):
+        self.write_form()
+
+    def post(self):
+        user_name = self.request.get("username")
+        user_pass = self.request.get("password")
+        user_verify = self.request.get("verify")
+        user_email = self.request.get("email")
+
+        if valid_username(user_name) and valid_password(user_pass) and valid_email(user_email) and user_pass == user_verify:
+            # redirect to success page
+            self.redirect("/success")
+        else:
+            if not valid_username(user_name):
+                pass
+            if not valid_password(user_pass):
+                pass
+            if not (user_pass == user_verify):
+                pass
+            if not valid_email(user_email):
+                pass
 
 
+class SuccessHandler(webapp2.RequestHandler):
+    def get(self, user = ""):
+        self.response.out.write(success % {"username": user})
 
 
-
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage), ('/success', SuccessHandler)], debug=True)
