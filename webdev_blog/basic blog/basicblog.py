e491a6107a8444a7e15cc20435269647dab1d667
Nathan Chang <changnat@usc.edu>
2018-01-01 19:27:18 -0800

    adding database queries

--- webdev_blog/basic blog/basicblog.py
+++ webdev_blog/basic blog/basicblog.py
@@ -19,8 +19,42 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+#the database
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+
+#class to handle adding new entries to the blog
+class NewEntry(Handler):
+    def get(self, subject="", content="", error=""):
+        self.render("submission.html", subject=subject, content=content, error=error)
+
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        if subject and content:
+            #create an entity
+            entry = Post(subject = subject, content = content)
+            #store it in the database
+            entry.put()
+            #and redirect to the permalink containing the post
+        else:
+            error = "subject AND content, por favor"
+            self.get(subject=subject, content=content, error=error)
 
 class MainPage(Handler):
+    #renders front page of blog, showing any entries in the database
+    def render_front(self):
+        entries = db.GqlQuery("SELECT * from Post "
+                           "ORDER BY created DESC ")
+
+        self.render("frontpage.html")
+
+    def get(self):
+        self.render_front()
 
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+app = webapp2.WSGIApplication([('/blog', MainPage), ('/blog/newpost', NewEntry)], debug=True)
+
+
